63ca889c89b7fe0839b11d75998172e2fc1e49aa
xiao32558152 <32558152@qq.com>
2017-07-06 23:13:19 +0800

    增加枚举

---
+++ application/lib/enum/TopicStatusEnum.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bnu
+ * Date: 2017/7/1
+ * Time: 21:47
+ */
+
+namespace app\lib\enum;
+
+
+class TopicStatusEnum
+{
+    const WAIT_ANSWER = 0;
+
+    const HAS_ANSWERER = 1;
+}
